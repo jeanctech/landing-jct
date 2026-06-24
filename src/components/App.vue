fb421be79b0bd23347319cfbbba35ab2776fa90b
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2026-06-23 23:11:43 -0500

    changes updates archives

---
+++ src/components/App.vue
@@ -0,0 +1,101 @@
+<script setup>
+// No script logic needed for this component
+</script>
+
+<template>
+  <main>
+    <header>
+      <div id="title-one">
+        <h1> 🚀 Bienvenido</h1><h2>A Un Server ✨ </h2>
+      </div>
+      <span id="hr">﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌</span>
+      <div id="title-two">
+        <h3><span id="span-one">( ＾◡＾)っ</span> De Discord</h3><h4><span id="span-one">(۶●ᴗ●)۶</span> Muy Chidooooo</h4>
+      </div>
+    </header>
+    <nav>
+      <div>
+        <p>
+          🤗 <b>E</b>ste es un servidor de Discord creado para la <br> <b>C</b>omunidad hispanohablante, donde podrás compartir 🤙
+        <br>
+        😆 <b>T</b>us intereses, hacer nuevos amigos y mucho mas
+        </p> <br> <span id="span-two">⸜(｡˃ ᵕ ˂ )⸝ </span> <br> <p id="description">
+           <b>¡Ú</b>nete a nosotros y sé
+         <br> <b>P</b>arte de esta increíble comunidad! 👇
+         <br>
+         <span id="span-one">⤵</span>
+        </p>
+      </div>
+    </nav>
+    <div class="main-div">
+      <a href="https://discord.gg/uTYCQqV4" target="_blank" rel="noopener noreferrer"> >..<- VER SERVER ʕ̢̣̣̣̣̩̩̩̩·͡˔·ོɁ̡̣̣̣̣̩̩̩̩✧</a>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#title-one {
+  text-align: center;
+  margin: 05px 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+#title-two {
+  text-align: center;
+  margin: 05px 0;
+  padding: 05px;
+  gap: 1rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+p {
+  text-align: center;
+  font-size: 1.2rem;
+  margin: 10px;
+  color: #dfdad9;
+}
+h1{color: #f6c177;}
+h2{color: #ea9a97;}
+#hr {
+    text-shadow:
+    0 0 10px #e0a332,
+    0 0 20px #dd7718,
+    0 0 40px #1d7681,
+    0 0 80px #0d8999,
+    0 0 160px #0d7481;
+}
+#span-one{
+  color: #a6da95;
+  font-size: 1.2rem;
+    text-shadow:
+    0 0 10px #ccc6c6,
+    0 0 20px #b8b3b3,
+    0 0 40px #5da7b1,
+    0 0 80px #4a97a1,
+    0 0 160px #3dadb9;
+}
+#span-two{
+  color: #ebe189;
+  font-size: 1.2rem;
+    text-shadow:
+    0 0 10px #ebe4e4,
+    0 0 20px #ece3e3,
+    0 0 40px #169daf,
+    0 0 80px #12acc0,
+    0 0 160px #10a6b9;
+}
+h3{
+  color: #ed8796;
+}
+h4{
+  color: #f6c177;
+}
+b{color: #7dc4e4; font-weight: bold; font-size: 1.5rem;}
+#description{
+  color: aquamarine;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './styles/style.css'
+import App from './components/App.vue'
+
+createApp(App).mount('#app')
